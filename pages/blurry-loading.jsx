6843ab8c98b0a3b7ca0be280f5bf632cc4271cc7
Greg Martin <gregm31@live.com>
2021-02-03 20:50:21 +1100

    Used some goddamn hook magic I got no clue about, will need to re-read article many times

--- pages/blurry-loading.jsx
+++ pages/blurry-loading.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
 import Head from "next/Head"
 
@@ -19,29 +19,45 @@ const BGSection = styled.section`
   left: -30px;
   width: calc(100vw + 60px);
   height: calc(100vh + 60px);
-  filter: blur(0px);
+  filter: blur(${props => `${props.blur}px`});
 `
 
 const LoadingText = styled.div`
   font-size: 50px;
   color: #fff;
   z-index: 1;
+  opacity: ${props => `${props.opacity}`};
 `
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef()
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
 
 const BlurryLoading = () => {
   const [loadCounter, setLoadCounter] = useState(0)
 
-  useState(() => {
-    let int = setInterval(blurring, 30)
-
-    const blurring = () => {
+  useInterval(
+    () => {
       setLoadCounter(loadCounter + 1)
-
-      if (loadCounter > 99) {
-        clearInterval()
-      }
-    }
-  }, [])
+    },
+    loadCounter > 99 ? null : 30
+  )
 
   const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
@@ -53,8 +69,13 @@ const BlurryLoading = () => {
         <title>Blurry Loading</title>
       </Head>
       <Body>
-        <BGSection BGImage="https://images.unsplash.com/photo-1576161787924-01bb08dad4a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2104&q=80"></BGSection>
-        <LoadingText>{`${loadCounter}%`}</LoadingText>
+        <BGSection
+          blur={scale(loadCounter, 0, 100, 30, 0)}
+          BGImage="https://images.unsplash.com/photo-1576161787924-01bb08dad4a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2104&q=80"
+        ></BGSection>
+        <LoadingText
+          opacity={scale(loadCounter, 0, 100, 1, 0)}
+        >{`${loadCounter}%`}</LoadingText>
       </Body>
     </>
   )
