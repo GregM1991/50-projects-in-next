c06bcfbb2a73aab689f44db5b4c7d9c906978ffe
Greg Martin <gregm31@live.com>
2021-02-04 08:25:29 +1100

    Added box styling and initial JS setup

--- pages/scroll-animation.jsx
+++ pages/scroll-animation.jsx
@@ -1,21 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Head from "next/head"
 
 const Body = styled.div`
-  background-color: #f6f7fb;
+  background-color: #efedd6;
   display: flex;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
-  height: 100vh;
-  overflow: hidden;
   margin: 0;
+
+  h1 {
+    margin: 10px;
+  }
 `
 
-const Box = styled.div``
+const Box = styled.div`
+  background-color: steelblue;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+  border-radius: 10px;
+  box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.3);
+  transform: ${props => (props.show ? "translateX(0)" : "translateX(400%)")};
+  transition: transform 0.4s ease;
+`
 
 const ScrollAnimation = () => {
+  const [show, setShow] = useState(false)
   return (
     <>
       <Head>
@@ -23,7 +39,7 @@ const ScrollAnimation = () => {
       </Head>
       <Body>
         <h1>Scroll to see the animation!</h1>
-        <Box>
+        <Box show={true}>
           <h2>Content</h2>
         </Box>
         <Box>
