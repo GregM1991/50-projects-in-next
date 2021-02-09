1032a58aa31d649d8d9d52e7dad857c5df0fab79
Greg Martin <gregm31@live.com>
2021-02-09 20:31:27 +1100

    Initial project setup

---
+++ pages/form-wave-animation.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+import Head from "next/head"
+
+const Body = styled.div`
+  background-color: #f6f7fb;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+`
+
+const ProjectTemplate = () => {
+  return (
+    <>
+      <Head>
+        <title>Form Wave Animation</title>
+      </Head>
+      <Body>
+        <h1>Hello</h1>
+      </Body>
+    </>
+  )
+}
+
+export default ProjectTemplate
