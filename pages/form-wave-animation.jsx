26d5dfc07450a9df3ffbc13675d01f30e254bd08
Greg Martin <gregm31@live.com>
2021-02-09 21:03:19 +1100

    HTML done and CSS started :

--- pages/form-wave-animation.jsx
+++ pages/form-wave-animation.jsx
@@ -3,7 +3,8 @@ import styled from "styled-components"
 import Head from "next/head"
 
 const Body = styled.div`
-  background-color: #f6f7fb;
+  background-color: steelblue;
+  color: #fff;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -12,6 +13,44 @@ const Body = styled.div`
   margin: 0;
 `
 
+const Container = styled.div`
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 20px 40px;
+  border-radius: 5px;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  a {
+    text-decoration: none;
+    color: lightblue;
+  }
+`
+
+const Form = styled.form``
+
+const FormControl = styled.div`
+  position: relative;
+  margin: 20px 0 40px;
+  width: 300px;
+
+  input {
+    background-color: transparent;
+    border: 0;
+    border-bottom: 2px #fff solid;
+    display: block;
+    width: 100%;
+    padding: 15px 0;
+    font-size: 18px;
+  }
+`
+
+const LoginButton = styled.button``
+
+const TextPara = styled.p``
+
 const ProjectTemplate = () => {
   return (
     <>
@@ -19,7 +58,22 @@ const ProjectTemplate = () => {
         <title>Form Wave Animation</title>
       </Head>
       <Body>
-        <h1>Hello</h1>
+        <Container>
+          <h1>Please Login</h1>
+          <Form>
+            <input type="text" required />
+            <label>Email</label>
+          </Form>
+          <FormControl>
+            <input type="password" required />
+            <label>Password</label>
+          </FormControl>
+
+          <LoginButton>Login</LoginButton>
+          <TextPara>
+            Don't have an account?<a href="#"> Register</a>
+          </TextPara>
+        </Container>
       </Body>
     </>
   )
